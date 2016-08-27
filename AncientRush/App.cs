d865ff447545514044e2954ad6a91b4c2411b32e
Dia6lo <Dgamjm@gmail.com>
2016-08-27 21:21:36 +1000

    Add scene classes. Merge all output scripts in AncientRush.js.

--- AncientRush/App.cs
+++ AncientRush/App.cs
@@ -1,39 +1,28 @@
-﻿using Bridge.Html5;
+﻿using AncientRush.Scenes;
+using Bridge.Html5;
 using Bridge.Pixi;
-using Bridge.Pixi.Interaction;
 
 namespace AncientRush
 {
     public class App
     {
-        private static Sprite sprite;
         private static IRenderer renderer;
+        private static Scene scene;
 
         [Ready]
         public static void Main()
         {
             renderer = Pixi.AutoDetectRenderer(800, 600, new RendererOptions {BackgroundColor = 0x1099bb});
             Document.Body.AppendChild(renderer.View);
-            var stage = new Container();
-            sprite = Sprite.FromImage("assets/Shia.png");
-            sprite.Position.Set(230, 264);
-            sprite.Interactive = true;
-            sprite.OnMouseDown(OnDown);
-            sprite.OnTouchStart(OnDown);
-            stage.AddChild(sprite);
+            scene = new MainMenu();
             Animate();
         }
 
         private static void Animate()
         {
             Window.RequestAnimationFrame(Animate);
-            renderer.Render(sprite);
-        }
-
-        private static void OnDown(InteractionEvent arg)
-        {
-            sprite.Scale.X = 0.3f;
-            sprite.Scale.Y = 0.3f;
+            scene.Update();
+            renderer.Render(scene.Container);
         }
     }
 }
---
+++ AncientRush/Scenes/Scene.cs
@@ -0,0 +1,16 @@
+﻿using Bridge.Pixi;
+
+namespace AncientRush.Scenes
+{
+    public abstract class Scene
+    {
+        protected Scene()
+        {
+            Container = new Container();
+        }
+
+        public Container Container { get; }
+
+        public abstract void Update();
+    }
+}
